7930f3b6d7668232f07e31e724c9897583c05502
yc <yc@yc>
2019-06-11 18:15:34 +0800

    export as default

--- server/http-request/http-request.ts
+++ server/http-request/http-request.ts
@@ -3,7 +3,21 @@ import * as axios from 'axios';
 import { Request, Response } from '../interface';
 
 function Get(req: Request, res: Response) {
-  axios.get(req.body.url).then(response => {
+  let url: string = req.body.url;
+  let headers = {};
+
+  if (req.body.headers) {
+    headers = {
+      headers: req.body.headers
+    };
+  }
+  if (req.body.parameters) {
+    url += '?';
+    for (let i = 0; i < req.body.parameters.length; ++i) {
+      url += req.body.parameters[i] + '=' + req.body.parameterValue[i] + '&';
+    }
+  }
+  axios.get(url, headers).then(response => {
     res.send(response.data);
   });
 }
