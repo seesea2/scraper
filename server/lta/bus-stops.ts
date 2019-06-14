89ce116e433d47eb861e845df4f2bc36fbf61794
yc <sgyc@yandex.com>
2019-06-15 00:10:18 +0800

    i

--- server/lta/bus-stops.ts
+++ server/lta/bus-stops.ts
@@ -55,4 +55,29 @@ function getBusStopInfo(busStopCode: string, res: Response) {
   return res.status(400).send('Bus Stop Info not found.');
 }
 
-export default getBusStopInfo;
+function getNearbyBusStops(latitude, longitude, res: Response) {
+  if (!latitude || !longitude) {
+    return res.status(400).send('Invalid coordinate.');
+  }
+
+  let nearbyStops = [];
+  const R = 6371;
+  for (let i = 0; i < busStops.length; i++) {
+    let dLat = ((busStops[i].Latitude - latitude) * Math.PI) / 180;
+    let dLong = ((busStops[i].Longitude - longitude) * Math.PI) / 180;
+
+    let a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos((busStops[i].Latitude * Math.PI) / 180) *
+        Math.cos((latitude * Math.PI) / 180) *
+        Math.sin(busStops[i].Longitude / 2) *
+        Math.sin(longitude / 2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    let dist = R * c * 1000;
+    if (Math.abs(dist) < 300) {
+      nearbyStops.push(busStops[i]);
+    }
+  }
+  res.status(200).send(nearbyStops);
+}
+export default { getBusStopInfo, getNearbyBusStops };
