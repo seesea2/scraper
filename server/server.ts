272e22f8211f3999e42f445537716b0b44033962
yc <seesea2@gmail.com>
2019-06-19 15:07:12 +0800

    debug server

--- server/server.ts
+++ server/server.ts
@@ -12,7 +12,8 @@ import apiRouter from './api-router';
 
 // Express server
 const app = express();
-const HOST = 'insg.xyz' // 'localhost';
+// const HOST = 'localhost';
+// const HOST = 'insg.xyz';
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -32,6 +33,14 @@ app.use(
 app.use('/api', apiRouter);
 
 app.get('/', (req: Request, res: Response) => {
+  console.log('in root');
+  console.log('req.baseUrl: ', req.baseUrl);
+  console.log('req.host: ', req.host);
+  console.log('req.hostname: ', req.hostname);
+  console.log('req.originalUrl: ', req.originalUrl);
+  console.log('req.path: ', req.path);
+  console.log('req.app: ', req.app);
+  console.log('req.url: ', req.url);
   return res.status(200).sendFile(join(__dirname, '/client/index.html'));
 });
 
@@ -52,10 +61,10 @@ const httpsOptions = {
   cert: fs.readFileSync(join(__dirname, '/cert/insg.crt')),
   key: fs.readFileSync(join(__dirname, '/cert/insg.key'))
 };
-app.set('domain', HOST);
+// app.set('domain', HOST);
 const https_server = https.createServer(httpsOptions, app);
 https_server.listen(443, () => {
-  console.log(`Node server listening on https://${HOST}:443`);
+  console.log(`Node server listening on https port 443`);
 });
 
 // redirect http request to https server
@@ -63,7 +72,7 @@ http
   .createServer((req: Request, res: Response) => {
     try {
       res.writeHead(301, {
-        Location: `https://${HOST}:443${req.url}`
+        Location: `https://${req.host}:443${req.url}`
       });
       res.end();
     } catch (e) {
