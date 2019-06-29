744eb0b26f8e5b12a48baeded1eb3b58a7c9e05a
yc <seesea2@gmail.com>
2019-06-29 17:37:54 +0800

    i

--- server/lunchfun-and-dictionary/dictionary.ts
+++ server/lunchfun-and-dictionary/dictionary.ts
@@ -1,26 +1,22 @@
-import * as OxfordDictionary from 'oxford-dictionary'; // tslint:disable-line
+import * as OxfordDictionary from 'oxford-dictionary-api'; // tslint:disable-line
 
 import { Response } from '../interface';
 
-const DICT = new OxfordDictionary({
-  app_id: '0314e9e2',
-  app_key: '5a6c2589474a2f83ccd69f397bfec7a2',
-  source_lang: 'en'
-});
+const app_id: string = '0314e9e2';
+const app_key: string = '5a6c2589474a2f83ccd69f397bfec7a2';
+const DICT = new OxfordDictionary(app_id, app_key);
 
 async function CheckOxfordDictionary(word: string, res: Response) {
   if (!word || !word.trim()) {
     return res.status(400).send('Invalid word.');
   }
 
-  DICT.definitions(word.trim()).then(
-    definitions => {
-      return res.status(200).send(definitions);
-    },
-    err => {
+  DICT.find(word.trim(), (err, data) => {
+    if (err) {
       return res.status(400).send(err);
     }
-  );
+    return res.status(200).send(data);
+  });
 }
 
 export default CheckOxfordDictionary;
