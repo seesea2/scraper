b7d415538287b62e5b142a9ef1bccb5fe523c20c
yc <seesea2@gmail.com>
2020-03-12 01:30:57 +0800

    ini

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatSliderModule } from '@angular/material/slider';
+
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faLeaf, faSearch, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+
 
 import { CoreModule } from '@core/core.module';
 
@@ -11,18 +16,22 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     CoreModule,
+    FontAwesomeModule,
     MatDividerModule,
+    MatSliderModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(faIconLib: FaIconLibrary) {
+    faIconLib.addIcons(faLeaf, faSearch, faChevronLeft);
+  }
+}
