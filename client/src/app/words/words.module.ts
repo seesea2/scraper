b83c8b6b8e144e4e4a574c1ce437ff6a75f20a05
yc <seesea2@gmail.com>
2020-03-24 01:48:01 +0800

    add frontend words module

---
+++ client/src/app/words/words.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WordsComponent } from './words.component';
+
+
+
+@NgModule({
+  declarations: [WordsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class WordsModule { }
